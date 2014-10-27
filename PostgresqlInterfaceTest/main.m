59d545d4c3428578f830a88a0b44dfbef1169b6b
Kiyoshi Nagahama <kiyoshin@me.com>
2014-10-27 17:22:30 -0600

    Commitment

--- PostgresqlInterfaceTest/main.m
+++ PostgresqlInterfaceTest/main.m
@@ -8,10 +8,30 @@
 
 #import <Foundation/Foundation.h>
 
+#include <libpq-fe.h>
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        const char *conninfo;
+        PGconn     *conn;
+        PGresult   *res;
+        int         nFields;
+        int         i,
+                    j;
+        
+        
+        conninfo = "host=192.168.1.185 port=5432 dbname=postgres user=postgres password=root";
+        conn = PQconnectdb(conninfo);
+        
+        if (PQstatus(conn) != CONNECTION_OK)
+        {
+            fprintf(stderr, "Connection to database failed: %s",
+                    PQerrorMessage(conn));
+        }
+        
+        int version = PQserverVersion(conn);
+        printf("Server version ... %i\n", version);
+        
     }
     return 0;
 }
